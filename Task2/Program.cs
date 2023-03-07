eaa842ca74cf77efd1620167d075edc3681767ff
David Bakalov <davidbakalov33@gmail.com>
2023-03-07 20:12:12 +0200

    sdsd

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -14,25 +14,46 @@ var generator = new MapGenerator(new MapGeneratorOptions()
 
 string[,] map = generator.Generate();
 new MapPrinter().Print(map,List);
+
 //Console.WriteLine("------------");
 //PaintBFS(new Point(6, 5));
 GetShortestPath(map,new Point(0,9),new Point(0,10));
 List<Point> GetShortestPath(string[,] map, Point start, Point goal)
 {
+   var MinimalDist = int.MaxValue;
     int[,] distances = new int[map.GetLength(0), map.GetLength(1)];
     Point[,] origins = new Point[map.GetLength(0), map.GetLength(1)];
-    var Q = new Queue<Point>();
+    var Q = new HashSet<Point>();
     // your code here
     foreach (Point p in convertToPoint(map))
     {
         distances[p.Row, p.Column] = 999999999;
         origins[p.Row, p.Column] = new Point(p.Column, p.Row);
-        Q.Enqueue(p);
+        Q.Add(p);
     }
+
+    distances[0, 9] = 0;
 
     while (Q.Count>0)
     {
-        
+        Point min = new Point();
+        foreach (Point v in Q)
+        {
+            if (distances[v.Row, v.Column] < MinimalDist)
+            {
+                MinimalDist = distances[v.Row, v.Column];
+                min = new Point(v.Column, v.Row);
+            }
+        }
+
+        Q.Remove(min);
+        foreach (var P in Q)
+        {
+            if (GetNeighbours(P.Column, P.Row, map, 1, true)!=null)
+            {
+                int alt = distances[min.Column,min.Row] + distances[GetNeighbours(P.Column, P.Row, map, 1, true)];
+            }
+        }
     }
 
     return null;
@@ -72,3 +93,27 @@ List<Point> convertToPoint(string[,] map)
 18
 19      return dist[], prev[]
 */
+
+ List<Point> GetNeighbours(int column, int row, string[,] maze, int offset = 2,
+    bool checkWalls = false)
+{
+    var result = new List<Point>();
+    TryAddWithOffset(offset, 0);
+    TryAddWithOffset(-offset, 0);
+    TryAddWithOffset(0, offset);
+    TryAddWithOffset(0, -offset);
+    return result;
+
+    void TryAddWithOffset(int offsetX, int offsetY)
+    {
+        var newColumn = column + offsetX;
+        var newRow = row + offsetY;
+        if (newColumn >= 0 && newRow >= 0 && newColumn < maze.GetLength(0) && newRow < maze.GetLength(1))
+        {
+            if (!checkWalls || maze[newColumn, newRow] == ".")
+            {
+                result.Add(new Point(newColumn, newRow));
+            }
+        }
+    }
+}
