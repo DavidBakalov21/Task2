bf5021ff9b51f125c11117f6043eb6f46b3ca496
David Bakalov <davidbakalov33@gmail.com>
2023-03-09 01:15:54 +0200

    sfsd

--- Task2/MapPointer.cs
+++ Task2/MapPointer.cs
@@ -5,26 +5,28 @@
 
     public class MapPrinter
     {
-        public void Print(string[,] maze, List<Point> path)
+        public void Print(string[,] maze, List<Point> path, List<Point> path2)
         {
-          
             PrintTopLine();
             for (var row = 0; row < maze.GetLength(1); row++)
             {
                 Console.Write($"{row}\t");
                 for (var column = 0; column < maze.GetLength(0); column++)
-                {
-                    if (column==path[0].Column && row==path[0].Row)
+                {  if (column==path[0].Column && row==path[0].Row)
                     {
                         Console.Write("A");
                     }else if (column==path[1].Column && row==path[1].Row)
                     {
                         Console.Write("B");
                     }
+                    else if (path2.Contains(new Point(column,row)))
+                    {
+                        Console.Write(".");
+                    }
                     Console.Write(maze[column, row]);
                 }
 
-                Console.WriteLine("L");
+                Console.WriteLine();
             }
 
             void PrintTopLine()
@@ -32,7 +34,7 @@
                 Console.Write($" \t");
                 for (int i = 0; i < maze.GetLength(0); i++)
                 {
-                    Console.Write(i % 10 == 0? i / 10 : "L");
+                    Console.Write(i % 10 == 0? i / 10 : " ");
                 }
     
                 Console.Write($"\n \t");
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,128 +1,141 @@
-﻿
-using Kse.Algorithms.Samples;
-
+﻿using Kse.Algorithms.Samples;
 var List = new List<Point>();
 List.Add(new Point(0,0));
-List.Add(new Point(15,5));
-
+List.Add(new Point(4,4));
 var generator = new MapGenerator(new MapGeneratorOptions()
 {
-    Height = 10,
-    Width = 10,
-    Noise = 0F
+    Height = 35,
+    Width = 90,
 });
 
 string[,] map = generator.Generate();
-new MapPrinter().Print(map,List);
+new MapPrinter().Print(map, List,FindShortestPath(map, List[0],List[1]));
 
-//Console.WriteLine("------------");
-PaintBFS(new Point(6, 5));
-//GetShortestPath(map,new Point(0,9),new Point(0,10));
-void PaintBFS(Point point)
-{
-    var visited = new List<Point>();
-    var queue = new Queue<Point>();
-    Visit(point);
-    queue.Enqueue(point);
-    while (queue.Count > 0)
+//FindShortestPath(map, List[0],List[1]);
+
+
+ List<Point> FindShortestPath(string[,] map, Point start, Point end)
     {
-        var next = queue.Dequeue();
-        new MapPrinter().Print(map,List);
-        var neighbours = GetNeighbours(next.Row, next.Column, map);
-        foreach (var neighbour in neighbours)
+        int numRows = map.GetLength(0);
+        int numCols = map.GetLength(1);
+        int[,] graph = new int[numRows * numCols, numRows * numCols];
+
+        for (int i = 0; i < numRows * numCols; i++)
         {
-            if (!visited.Contains(neighbour))
+            for (int j = 0; j < numRows * numCols; j++)
             {
-                Visit(neighbour);
-                queue.Enqueue(neighbour);
+                graph[i, j] = 999999999;
             }
         }
-    }
 
-    void Visit(Point point)
-    {
-        map[point.Row, point.Column] = "1";
-        visited.Add(point);
-    }
-}
-
-List<Point> GetShortestPath(string[,] map, Point start, Point goal)
-{
-    HashSet<Point> Points = new HashSet<Point>();
-    foreach (Point value in convertToPoint(map))
-    {
-        Points.Add(new Point(value.Column, value.Row));
-    }
-    var visited = new List<Point>();
-    
-    void Visit(Point point)
-    {
-        map[point.Column, point.Row] = "f";
-        visited.Add(point);
-    }
-
-    return null;
-}
-
-List<Point> convertToPoint(string[,] map)
-{
-    var ToPoint = new List<Point>();
-    for (var row = 0; row < map.GetLength(1); row++)
-    {
-     //   Console.Write($"{row}\t");
-        for (var column = 0; column < map.GetLength(0); column++)
+        for (int row = 0; row < numRows; row++)
         {
-            if (map[column, row] != "█")
+            for (int col = 0; col < numCols; col++)
             {
-                ToPoint.Add(new Point(column,row));  
-            }
-            
-        }
-    }
+                if (map[row, col] == "█")
+                {
+                    continue;
+                }
+//index of node
+                int node1 = row * numCols + col;
+                //index of node
+                if (row > 0 && map[row - 1, col] != "█")
+                {
+                    int node2 = (row - 1) * numCols + col;
+                    graph[node1, node2] = 1;
+                    graph[node2, node1] = 1;
+                }
 
-    return ToPoint;
-}
-/*function Dijkstra(Graph, source):
-2      
-3      for each vertex v in Graph.Vertices:
-4          dist[v] ← INFINITY
-5          prev[v] ← UNDEFINED
-6          add v to Q
-7      dist[source] ← 0
-8      
-9      while Q is not empty:
-10          u ← vertex in Q with min dist[u]
-11          remove u from Q
-12          
-13          for each neighbor v of u still in Q:
-14              alt ← dist[u] + Graph.Edges(u, v)
-15              if alt < dist[v]:
-16                  dist[v] ← alt
-17                  prev[v] ← u
-18
-19      return dist[], prev[]
-*/
+                if (row < numRows - 1 && map[row + 1, col] != "█")
+                {
+                    int node2 = (row + 1) * numCols + col;
+                    graph[node1, node2] = 1;
+                    graph[node2, node1] = 1;
+                }
 
- List<Point> GetNeighbours(int column, int row, string[,] maze, int offset = 2,
-    bool checkWalls = false)
-{
-    var result = new List<Point>();
-    TryAddWithOffset(offset, 0);
-    TryAddWithOffset(-offset, 0);
-    TryAddWithOffset(0, offset);
-    TryAddWithOffset(0, -offset);
-    return result;
+                if (col > 0 && map[row, col - 1] != "█")
+                {
+                    int node2 = row * numCols + col - 1;
+                    graph[node1, node2] = 1;
+                    graph[node2, node1] = 1;
+                }
 
-    void TryAddWithOffset(int offsetX, int offsetY)
-    {
-        var newColumn = column + offsetX;
-        var newRow = row + offsetY;
-        if (newColumn >= 0 && newRow >= 0 && newColumn < maze.GetLength(0) && newRow < maze.GetLength(1))
-        {
-            if (!checkWalls || maze[newColumn, newRow] == ".")
-            {
-                result.Add(new Point(newColumn, newRow));
+                if (col < numCols - 1 && map[row, col + 1] != "█")
+                {
+                    int node2 = row * numCols + col + 1;
+                    graph[node1, node2] = 1;
+                    graph[node2, node1] = 1;
+                }
             }
         }
+
+        int startNode = start.Row * numCols + start.Column;
+        int endNode = end.Row * numCols + end.Column;
+
+        int[] distances = new int[numRows * numCols];
+        bool[] visited = new bool[numRows * numCols];
+        int[] previous = new int[numRows * numCols];
+
+        for (int i = 0; i < numRows * numCols; i++)
+        {
+            distances[i] = int.MaxValue;
+            visited[i] = false;
+            previous[i] = -1;
+        }
+
+        distances[startNode] = 0;
+
+        for (int i = 0; i < numRows * numCols - 1; i++)
+        {
+            int current = MinDistance(distances, visited);
+            visited[current] = true;
+
+            for (int j = 0; j < numRows * numCols; j++)
+            {
+                if (!visited[j] && graph[current, j] != int.MaxValue && distances[current] + graph[current, j] < distances[j])
+                {
+                    distances[j] = distances[current] + graph[current, j];
+                    previous[j] = current;
+                }
+            }
+        }
+
+        List<Point> path = new List<Point>();
+
+        int node = endNode;
+        while (previous[node] != -1)
+        {
+            int row = node / numCols;
+            int col = node % numCols;
+            path.Insert(0, new Point(col, row));
+            node = previous[node];
+        }
+
+        path.Insert(0, start);
+        
+        foreach (var point in path)
+        {
+            Console.WriteLine("({0}, {1})", point.Row, point.Column);
+        }
+       int MinDistance(int[] distances, bool[] visited)
+        {
+            int min = 999999999;
+            int minIndex = 0;
+
+            for (int i = 0; i < distances.Length; i++)
+            {
+                if (!visited[i] && distances[i] <= min)
+                {
+                    min = distances[i];
+                    minIndex = i;
+                }
+            }
+
+            return minIndex;
+        }
+
+        return path;
     }
-}
+
+
+
