a841d1ed00d24831b81e6eb6492f138fd897f950
David Bakalov <davidbakalov33@gmail.com>
2023-03-10 11:17:59 +0200

    Asus-Wins Mac-sucks

--- Task2/Point.cs
+++ Task2/Point.cs
@@ -1,12 +1,11 @@
-﻿
-namespace Kse.Algorithms.Samples
+﻿namespace Kse.Algorithms.Samples
 {
     public struct Point
     {
         public int Column;
         public int Row;
 
-        public Point( int row,int column)
+        public Point( int column,int row)
         {
             Column = column;
             Row = row;
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,16 +1,17 @@
 ﻿using Kse.Algorithms.Samples;
 var List = new List<Point>();
 List.Add(new Point(0,0));
-List.Add(new Point(8,7));
+List.Add(new Point(88,32));
 var generator = new MapGenerator(new MapGeneratorOptions()
 {
     Height = 35,
     Width = 90,
+    Seed = 1
 });
 
 string[,] map = generator.Generate();
-map[List[0].Row, List[0].Column] = " ";
-map[List[1].Row, List[1].Column] = " ";
+map[List[0].Column, List[0].Row] = " ";
+map[List[1].Column, List[1].Row] = " ";
 new MapPrinter().Print(map, List,FindShortestPath(map, List[0],List[1]));
 List<Point> FindShortestPath(string[,] map, Point start, Point end)
     {
@@ -21,7 +22,7 @@ List<Point> FindShortestPath(string[,] map, Point start, Point end)
         
         var previous = new Dictionary<Point, Point>();
         previous[state] = state;
-        while (state.Row!=end.Row || state.Column!=end.Column)
+        while (!(state.Row==end.Row && state.Column==end.Column))
         {
             var neoghbors = GetNeigbors(state, map);
             foreach (var VAR in neoghbors)
@@ -30,18 +31,17 @@ List<Point> FindShortestPath(string[,] map, Point start, Point end)
                 {
                     distances[VAR] = distances[state]+1;
                     previous[VAR] = state;
-                    
                 }
             }
             distances.Remove(state);
             if (distances.Count==0)
             {
-                break;  
+                Console.WriteLine("BRUH");
+                break;
             }
             state=distances.MinBy(kvp => kvp.Value).Key;
-           
-           
         }
+        
         List<Point> path = new List<Point>();
         while (state.Row!=start.Row || state.Column!=start.Column)
         {
@@ -52,40 +52,37 @@ List<Point> FindShortestPath(string[,] map, Point start, Point end)
 
         foreach (var VARIABLE in path)
         {
-            Console.WriteLine("({0},{1})",VARIABLE.Row,VARIABLE.Column);
+            Console.WriteLine("({0},{1})",VARIABLE.Column,VARIABLE.Row);
         }
         return path;
     }
 
 
-List<Point> GetNeigbors(Point start,string[,] map)
+List<Point> GetNeigbors(Point start, string[,] map)
 {
-    int numRows = map.GetLength(0);
-    int numCols = map.GetLength(1);
+    int numRows = map.GetLength(1);
+    int numCols = map.GetLength(0);
     var Neighbor = new List<Point>();
     //index of node
-    if (start.Row > 0 && map[start.Row - 1, start.Column] != "█")
+    if (start.Row > 0 && map[start.Column, start.Row - 1] != "█")
     {
         Point node2 = new Point(start.Column, start.Row - 1);
         Neighbor.Add(node2);
-               
     }
 
-    if (start.Row < numRows - 1 && map[start.Row + 1, start.Column] != "█")
+    if (start.Row < numRows - 1 && map[start.Column, start.Row + 1] != "█")
     {
         Point node2 = new Point(start.Column, start.Row + 1);
         Neighbor.Add(node2);
-             
     }
 
-    if (start.Column > 0 && map[start.Row, start.Column - 1] != "█")
+    if (start.Column > 0 && map[start.Column - 1, start.Row] != "█")
     {
         Point node2 = new Point(start.Column - 1, start.Row);
         Neighbor.Add(node2);
-              
     }
 
-    if (start.Column < numCols - 1 && map[start.Row, start.Column + 1] != "█")
+    if (start.Column < numCols - 1 && map[start.Column + 1, start.Row] != "█")
     {
         Point node2 = new Point(start.Column + 1, start.Row);
         Neighbor.Add(node2);
